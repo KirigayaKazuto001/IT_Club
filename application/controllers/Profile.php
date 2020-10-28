8d1147ca1a8fffaa92586edda52189cd21caaaec
KirigayaKazuto001 <72965844+KirigayaKazuto001@users.noreply.github.com>
2020-10-28 10:34:31 +0700

    Add menu

---
+++ application/controllers/Profile.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('template/header');
+        $this->load->view('profile');
+        $this->load->view('template/footer');
+    }
+
+    public function organisasi() 
+    {
+        $this->load->view('template/header');
+        $this->load->view('organisasi');
+        $this->load->view('template/footer');
+    }
+}
